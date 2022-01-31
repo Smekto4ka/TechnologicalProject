7f85330ff3178f0b5ba14b82c90f05f1a2ba0b8c
Smekto4ka <barsalekcandr@mail.ru>
2022-01-31 22:58:03 +0300

    sketch, not that

---
+++ News Server/backend/src/main/java/love/smekto4ka/dto/UserProfile.java
@@ -0,0 +1,13 @@
+package love.smekto4ka.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserProfile {
+    private String name;
+    private String email;
+}
